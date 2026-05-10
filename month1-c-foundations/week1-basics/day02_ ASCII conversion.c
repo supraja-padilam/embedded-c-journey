88432e5479e6178a5b2c0128d4c7a59b0983469d
supraja-padilam <suprajapadilam@gmail.com>
2026-05-10 22:48:45 +0530

    Update day02_ ASCII conversion.c

--- month1-c-foundations/week1-basics/day02_ ASCII conversion.c
+++ month1-c-foundations/week1-basics/day02_ ASCII conversion.c
@@ -1,18 +1,13 @@
-// Online C compiler to run C program online
 #include <stdio.h>
 int a;
 char b;
 int main() {
-    // Write C code here
+    
     printf("enter int value (0-127) for ASCII :");
     scanf("%d",&a);
-    
     printf("ASCII value of integer is :%c\n",a);
-    
     printf("Enter a character: ");
-    
     scanf(" %c",&b);
-    
     printf("Integer value of ASCII Value is :%d\n",b);
     
     return 0;
